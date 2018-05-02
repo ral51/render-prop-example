00f28344d1045d61470ffa3e2f13f7ac2e071074
odalay <oraldalay@gmail.com>
2018-05-02 13:49:34 -0400

    first check in

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StaticRenderer } from './StaticRenderer';
+
+export default function App() {
+	function render() {
+		return (<div> Hello World </div>);
+	}
+
+	return (
+		<StaticRenderer render={render} />
+	);
+}
---
+++ src/StaticRenderer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export class StaticRenderer extends Component {
+
+	render() {
+		return (
+			<div>
+				{ this.props.render() }
+			</div>
+		)
+
+	}
+
+}
